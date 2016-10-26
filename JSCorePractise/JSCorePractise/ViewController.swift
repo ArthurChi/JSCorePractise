63a4cf42b28f4016ea8ae26c5b3a176671d3da5a
cjfire <cjfire@sina.cn>
2016-10-26 17:03:11 +0800

    Create project

---
+++ JSCorePractise/JSCorePractise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSCorePractise
+//
+//  Created by cjfire on 16/10/26.
+//  Copyright © 2016年 cjfire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
