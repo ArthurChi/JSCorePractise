55c3d4df03c6258df623e4dfad3938017f98fd8c
cjfire <cjfire@sina.cn>
2016-10-27 16:44:39 +0800

    wkwebview demo

--- JSCorePractise/JSCorePractise/WKWebViewController.swift
+++ JSCorePractise/JSCorePractise/WKWebViewController.swift
@@ -7,13 +7,43 @@
 //
 
 import UIKit
+import WebKit
 
 class WKWebViewController: UIViewController {
 
+    var contentController = WKUserContentController()
+    
+    lazy var webView: WKWebView = {
+        let config = WKWebViewConfiguration()
+        config.userContentController = self.contentController
+        let webView = WKWebView(frame: CGRect.zero, configuration: config)
+        return webView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        webView.frame = view.bounds
+        view.addSubview(webView)
+        
+        let path = Bundle.main.path(forResource: "JSCorePractise", ofType: "html")
+        let url = URL(fileURLWithPath: path!)
+        let req = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 30)//URLRequest(url: url)
+        webView.load(req)
+        
+        let userScript = WKUserScript(source: "redHeader()", injectionTime: WKUserScriptInjectionTime.atDocumentEnd, forMainFrameOnly: true)
+        contentController.addUserScript(userScript)
+        
+        contentController.add(self, name: "callbackHandler")
     }
+}
 
+extension WKWebViewController: WKScriptMessageHandler {
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        
+        if message.name == "callbackHandler" {
+            print("message is \(message.body)")
+        }
+    }
 }
